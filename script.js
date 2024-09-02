83d7c2d3e5d7928cc0b207f2ffa4b038b94fb5e6
PlingifyPlug ("Vinti") <168746576+BackupPASS@users.noreply.github.com>
2024-09-02 01:31:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,16 @@
     
         if (userAgent.indexOf('Windows') !== -1) {
             document.getElementById('windows-notification').style.display = 'flex';
-        } else if (userAgent.indexOf('Mac') !== -1 && userAgent.indexOf('iPhone') === -1) {
-            document.getElementById('mac-notification').style.display = 'flex';
         } else if (userAgent.indexOf('iPhone') !== -1) {
             document.getElementById('iphone-notification').style.display = 'flex';
         } else if (userAgent.indexOf('Android') !== -1) {
             document.getElementById('android-notification').style.display = 'flex';
-        }
+        } else {
+          document.getElementById('unknown-notification').style.display = 'flex';
+      }
+
     }
+    
     
 
     showPlatformNotification();
@@ -44,3 +46,40 @@
         setTimeout(showCookieNotice, 1000);
       
         document.cookie = "username=JohnDoe; path=/; secure; HttpOnly";
+
+
+        let currentIndex = 0;
+const images = [
+
+    'image2.jpg',
+
+];
+const slideshow = document.getElementById('background-slideshow');
+
+function changeBackground(index) {
+    slideshow.style.backgroundImage = `url(${images[index]})`;
+    currentIndex = index;
+}
+
+changeBackground(0);
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const helpButton = document.getElementById('helpButton');
+  const helpBox = document.getElementById('helpBox');
+  const closeButton = document.getElementById('closeButton');
+
+  helpButton.addEventListener('click', function() {
+      if (helpBox.style.display === 'block') {
+          helpBox.style.display = 'none';
+      } else {
+          helpBox.style.display = 'block';
+      }
+  });
+
+  closeButton.addEventListener('click', function() {
+      helpBox.style.display = 'none';
+  });
+});
