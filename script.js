d51e2f579ef5eccce06181ce1ab8c6020ad2b87c
PlingifyPlug ("Vinti") <168746576+BackupPASS@users.noreply.github.com>
2025-01-24 01:14:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,209 @@
-   
-    function showPlatformNotification() {
-        const userAgent = navigator.userAgent;
-    
-        if (userAgent.indexOf('Windows') !== -1) {
-            document.getElementById('windows-notification').style.display = 'flex';
-        } else if (userAgent.indexOf('Mac') !== -1 && userAgent.indexOf('iPhone') === -1) {
-              document.getElementById('mac-notification').style.display = 'flex';
-        } else if (userAgent.indexOf('iPhone') !== -1) {
-            document.getElementById('iphone-notification').style.display = 'flex';
-        } else if (userAgent.indexOf('Android') !== -1) {
-            document.getElementById('android-notification').style.display = 'flex';
-          } else if (userAgent.indexOf('CrOS') !== -1) {  
-            document.getElementById('chromebook-notification').style.display = 'flex';
-        } else if (userAgent.indexOf('Linux') !== -1 && userAgent.indexOf('Android') === -1) { 
-            document.getElementById('linux-notification').style.display = 'flex';
-        } else {
-          document.getElementById('unknown-notification').style.display = 'flex';
-      }
-
-    }
-    
-    
-
-    showPlatformNotification();
-
-    setTimeout(function() {
-        showCookieNotice();
-      }, 1000);
-      
-      
-        function hasAcceptedCookies() {
-          return document.cookie.split(';').some((item) => item.trim().startsWith('cookieAccepted='));
-        }
-      
-        function showCookieNotice() {
-          if (!hasAcceptedCookies()) {
-            document.getElementById('cookie-card').style.display = 'block';
-          }
-        }
-      
-        function acceptCookies() {
-          document.getElementById('cookie-card').style.display = 'none'; 
-      
-          document.cookie = 'cookieAccepted=true; max-age=31536000'; 
-        }
-      
-      
-        document.getElementById('accept-cookies').addEventListener('click', acceptCookies);
-      
-      
-        setTimeout(showCookieNotice, 1000);
-      
-        document.cookie = "username=JohnDoe; path=/; secure; HttpOnly";
+const notificationContainer = document.getElementById('notification-container');
+const verificationCard = document.getElementById('verification-card');
+const verifyBtn = document.getElementById('verify-btn');
+const verifyInput = document.getElementById('verify-input');
+ const verifyResult = document.getElementById('verify-result');
+let downloadBtn;
+function createNotificationCard(platform, title, desc, downloadLink, requirementsLink) {
+   const card = document.createElement('div');
+   card.className = 'card';
+   card.id = `${platform}-notification`;
 
 
-        let currentIndex = 0;
-const images = [
+  const icon = document.createElement('div');
+   icon.className = 'icon';
+   icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <g id="SVGRepo_iconCarrier">
+         <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#ffffff" d="M20 14V17.5C20 20.5577 16 20.5 12 20.5C8 20.5 4 20.5577 4 17.5V14M12 15L12 3M12 15L8 11M12 15L16 11"></path>
+    </g>
+  </svg>`;
 
-    'image2.jpg',
+   const content = document.createElement('div');
+   content.className = 'content';
+   content.innerHTML = `<span class="title">${title}</span>
+                      <div class="desc">${desc}</div>`;
 
-];
-const slideshow = document.getElementById('background-slideshow');
-
-function changeBackground(index) {
-    slideshow.style.backgroundImage = `url(${images[index]})`;
-    currentIndex = index;
-}
-
-changeBackground(0);
-
-
-
-
-function checkSpeed() {
-
-  var testImageUrl = 'image2.jpg';
-  var startTime, endTime;
+  const actions = document.createElement('div');
+  actions.className = 'actions';
 
 
-  function speedTest() {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', testImageUrl, true);
-      xhr.responseType = 'blob';
-      
-      xhr.onloadstart = function () {
-          startTime = new Date().getTime();
-      };
+  if (downloadLink) {
+     actions.innerHTML += `<div><a href="${downloadLink}" class="download">Download Vinti</a></div>`;
+   }
 
-      xhr.onload = function () {
-          endTime = new Date().getTime();
-          var duration = (endTime - startTime) / 1000; // seconds
-          var fileSize = xhr.response.size / 1024 / 1024; // MB
-          var speedMbps = (fileSize * 8) / duration; // Mbps
-
-        
-          if (speedMbps < 10) {
-              window.location.href = 'https://backuppass.github.io/Slow-Wifi';
-          }
-      };
-
-      xhr.onerror = function () {
-          window.location.href = 'https://backuppass.github.io/Site-Crashed';
-      };
-
-      xhr.send();
+   if (requirementsLink) {
+       actions.innerHTML += `<div><a href="${requirementsLink}" class="notnow">To view requirements click here.</a></div>`;
   }
 
+   const closeButtonContainer = document.createElement('a')
+   closeButtonContainer.href = 'https://plingifyplug.com';
+   const closeButton = document.createElement('button');
+  closeButton.type = 'button';
+   closeButton.className = 'close';
+  closeButton.innerHTML = `<svg aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+     </svg>`;
 
-  speedTest();
+
+   closeButtonContainer.appendChild(closeButton);
+
+   content.appendChild(actions);
+  card.appendChild(icon);
+   card.appendChild(content);
+   card.appendChild(closeButtonContainer)
+    return card
+}
+function showPlatformNotification() {
+    const userAgent = navigator.userAgent;
+   let notification = null;
+   let downloadLink = "";
+    if (userAgent.indexOf('Windows') !== -1) {
+       downloadLink = 'https://www.mediafire.com/file/sp5ugl8ycbet2vf/Vinti_2.0.msi/file';
+       notification = createNotificationCard('windows', 'Windows Users', 'Vinti', downloadLink, 'https://plingifyplug.com/VintiRequirements');
+    } else if (userAgent.indexOf('Mac') !== -1 && userAgent.indexOf('iPhone') === -1) {
+       notification = createNotificationCard('mac', 'Mac Users', 'Vinti is coming soon for MacOS 11.7.10 BigSur.', null, 'https://plingifyplug.com/VintiRequirements');
+    } else if (userAgent.indexOf('iPhone') !== -1) {
+       notification = createNotificationCard('iphone', 'iPhone Users', 'This software is not available for download on iPhone.', null, 'https://plingifyplug.com');
+    } else if (userAgent.indexOf('Android') !== -1) {
+        notification = createNotificationCard('android', 'Android Users', 'This software is not available for download on Android.', null, 'https://plingifyplug.com');
+    } else if (userAgent.indexOf('CrOS') !== -1) {
+       notification = createNotificationCard('chromebook', 'Chromebook Users', 'This software is not available for download on Chromebooks.', null, 'https://plingifyplug.com');
+  } else if (userAgent.indexOf('Linux') !== -1 && userAgent.indexOf('Android') === -1) {
+        notification = createNotificationCard('linux', 'Linux Users', 'This software is not available for download on Linux.', null, 'https://plingifyplug.com');
+   }else{
+       notification = createNotificationCard('unknown', 'Unsupported Device', 'This software is not available on your device.', null, null);
+   }
+
+    notificationContainer.appendChild(notification);
+  notification.classList.add('card-show');
+
+  if (downloadLink != ""){
+    downloadBtn = document.querySelector('#windows-notification .download');
+      downloadBtn.addEventListener('click', function(event){
+          event.preventDefault();
+         startVerification(downloadLink);
+      });
+  }
+
+}
+showPlatformNotification();
+setTimeout(function () {
+    showCookieNotice();
+}, 1000);
+ function hasAcceptedCookies() {
+   return document.cookie.split(';').some((item) => item.trim().startsWith('cookieAccepted='));
+}
+function showCookieNotice() {
+    if (!hasAcceptedCookies()) {
+      document.getElementById('cookie-card').style.display = 'block';
+   }
+}
+function acceptCookies() {
+  document.getElementById('cookie-card').style.display = 'none';
+
+    document.cookie = 'cookieAccepted=true; max-age=31536000';
 }
 
+document.getElementById('accept-cookies').addEventListener('click', acceptCookies);
+setTimeout(showCookieNotice, 1000);
 
-window.onload = function() {
-  checkSpeed();
-};
+document.cookie = "username=JohnDoe; path=/; secure; HttpOnly";
+
+let currentIndex = 0;
+const images = [
+   'image2.jpg',
+];
+
+const slideshow = document.getElementById('background-slideshow');
+function changeBackground(index) {
+    slideshow.style.backgroundImage = `url(${images[index]})`;
+   currentIndex = index;
+}
+changeBackground(0);
+function checkSpeed() {
+    var testImageUrl = 'image2.jpg';
+   var startTime, endTime;
+
+    function speedTest() {
+      var xhr = new XMLHttpRequest();
+        xhr.open('GET', testImageUrl, true);
+      xhr.responseType = 'blob';
+
+      xhr.onloadstart = function () {
+         startTime = new Date().getTime();
+       };
+
+        xhr.onload = function () {
+         endTime = new Date().getTime();
+           var duration = (endTime - startTime) / 1000;
+           var fileSize = xhr.response.size / 1024 / 1024;
+           var speedMbps = (fileSize * 8) / duration;
+        if (speedMbps < 5) {
+              window.location.href = 'https://backuppass.github.io/Slow-Wifi';
+         }
+       };
+     xhr.onerror = function () {
+        window.location.href = 'https://backuppass.github.io/Site-Crashed';
+    };
+        xhr.send();
+   }
+
+   speedTest();
+}
+
+window.onload = function () {
+    checkSpeed();
+ };
+let verificationActive = false;
+let verificationTimer;
+ function startVerification(link){
+        const notification = createNotificationCard('ping', "Security Check", "Please be ready for a small verification check", null, null);
+           notificationContainer.appendChild(notification);
+      notification.classList.add('card-show');
+     setTimeout(function(){
+             verificationActive = true;
+            verificationCard.style.display = "flex";
+            verificationTimer = setTimeout( function() {
+             verifyInput.value = "";
+             verifyResult.style.display = "flex"
+              verifyResult.style.color = "red"
+               verifyResult.innerHTML = "Verification Failed please try again"
+               verificationActive = false
+               },10000);
+      },1000)
+
+       function checkVerification(){
+    if(verifyInput.value == "3$7hJKl2@" && verificationActive == true)
+      {
+          clearTimeout(verificationTimer)
+            verificationActive = false;
+             verifyResult.style.display = "flex";
+             verifyResult.style.color = "green";
+           verifyResult.innerHTML = "Verification Completed!"
+           window.location.href = link;
+        verificationCard.style.display = "none";
+      }
+       else if (verificationActive == false){
+          verifyResult.style.display = "flex"
+        verifyResult.style.color = "red"
+        verifyResult.innerHTML = "Verification failed due to no time or incorrect Input";
+
+       }
+     else{
+       verifyResult.style.display = "flex"
+          verifyResult.style.color = "red"
+            verifyResult.innerHTML = "Incorrect verification code try again!"
+    }
+ }
+
+    verifyBtn.addEventListener('click', function(){
+        checkVerification()
+    });
+  verifyInput.addEventListener("keyup", function(event) {
+     if (event.key === "Enter") {
+         event.preventDefault();
+          checkVerification();
+       }
+    });
+
+}
