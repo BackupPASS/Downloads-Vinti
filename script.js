eef21b7e5ed760f51b21a6e9ff9268260809e464
PlingifyPlug ("Vinti") <168746576+BackupPASS@users.noreply.github.com>
2025-03-16 23:02:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function showPlatformNotification() {
    let notification = null;
    let downloadLink = "";
     if (userAgent.indexOf('Windows') !== -1) {
-       downloadLink = 'https://www.mediafire.com/file/365zgnsakn8jmfl/Vinti.msi/file';
+       downloadLink = 'https://www.mediafire.com/file/lz8my4rs1i9zf75/Vinti_Setup.msi/file';
        notification = createNotificationCard('windows', 'Windows Users', 'Vinti Windows 10+', downloadLink, 'https://plingifyplug.com/VintiRequirements');
     } else if (userAgent.indexOf('Mac') !== -1 && userAgent.indexOf('iPhone') === -1) {
      DownloadLink = 'https://www.mediafire.com/file/g7b0to2sp4nqgb6/Vinti_MacOS.app.zip/file';
