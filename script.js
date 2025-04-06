a8239f1068dc99522832f524284f3b83180f104a
PlingifyPlug ("Vinti") <168746576+BackupPASS@users.noreply.github.com>
2025-04-06 23:43:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function showPlatformNotification() {
         downloadButtonText = 'Download Vinti';
         notification = createNotificationCard('windows', 'Windows Users', 'Vinti Windows 10+', downloadLink, 'https://plingifyplug.com/VintiRequirements', downloadButtonText);
     } else if (userAgent.indexOf('Mac') !== -1 && userAgent.indexOf('iPhone') === -1) {
-        downloadLink = 'https://backuppass.github.io';
+        downloadLink = 'https://github.com/BackupPASS/Downloads/raw/refs/heads/main/Vinti%20MacOS.app.zip';
         downloadButtonText = 'Download Vinti'; 
         notification = createNotificationCard('mac', 'Mac Users', 'Vinti MacOS BigSur 11.7.10+', downloadLink, 'https://plingifyplug.com/VintiRequirements', downloadButtonText);
      } else if (userAgent.indexOf('iPhone') !== -1) {
